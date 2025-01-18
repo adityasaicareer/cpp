0ef0971216d1c6af99e04ba09c61d03778219128
Adityasai <adityasaicareer@gmail.com>
2025-01-18 18:09:18 +0530

    whileloop

---
+++ whileloop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        cout<<"aditya"<<" "<<i<<endl;
+        i=i+1;
+    }
+
+    int j=1;
+    do{
+        cout<<"value "<<j<<endl;
+       
+    }while(j<1);
+
+
+}
